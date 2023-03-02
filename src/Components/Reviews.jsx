22d31390f186a2b89154c9b464bf83c3e56f91d2
Moises Martinez <moisesmartinez@MacBook-Pro.local>
2023-03-02 11:58:54 -0600

    made my first commit

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,7 +1,12 @@
+import "./Review.css";
+
 function Reviews() {
-    return ( 
-        <p>Reviews Component</p>
-     );
+  return (
+    <>
+      <p>Reviews Component</p>
+      <h1> i made my fist commit </h1>
+    </>
+  );
 }
 
 export default Reviews;
