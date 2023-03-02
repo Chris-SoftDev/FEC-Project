b00d07be01c79d6e182ea531d0d420393595ff39
Sounisa <119640964+sounisa@users.noreply.github.com>
2023-03-02 13:13:25 -0800

    starting host

--- src/Components/Host.jsx
+++ src/Components/Host.jsx
@@ -2,7 +2,45 @@ import './Host.css'
 
 function Host() {
     return ( 
-        <p>Host Component</p>
+        <div>
+            <div className = "myhost-container">
+                <div className = "host-cohost">
+                    <div className ="host">
+                        <div className= "host-img-container">
+                            <img className ="host-img">{/*DATA.img*/}</img>
+                        </div>
+                        <div className ="host-name">
+                            <div className ="hosted-by">Hosted by Tag & Sherilyn</div>
+                            {/*<div className ="hosted-by">Hosted by {DATA.host} & {DATA.cohost}</div>*/}
+                            <div className= "month-year">Joined in November 2017</div>
+                            {/*<div className= "month-year">Joined in {DATA.month} {DATA.year}</div>*/}
+                        </div>
+                    </div>
+                    <div className ="cohost">
+												<div>
+													
+												</div>
+                    </div>
+                </div>
+                <div className = "contact-host">
+                    <ul className ="contact">
+                        <li>Language: English</li>
+                        <li>Response rate: 100%</li>
+                        <li>Response time: within a few hours</li>
+                    </ul>
+										<div className= "contact-btn-div">
+                    	<button className= "contact-btn">Contact Host</button>
+										</div>
+										<div className ="payment-protect">
+                        <div>LOGO</div>
+                        <p>To protect your payment, never transfer money or communicate outside of the Airbnb website or app.</p>
+                    </div>
+                </div>
+            </div>
+            <div className ='thingstoknow'>
+                <div>Things to know</div>
+            </div>
+        </div>
      );
 }
 
