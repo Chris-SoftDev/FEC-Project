6df17bff42451d4e52adcd674639c9746837cdb9
Moises Martinez <moisesmartinez@MacBook-Pro.local>
2023-03-02 21:25:32 -0600

    category adjustment

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -3,24 +3,73 @@ import "./Review.css";
 function Reviews() {
   return (
     <div className="Review-Container">
-      <p className="Total-rating-reviews">* 4.93 210 reviews</p>
+      <p className="Total-rating-reviews">
+        <div className="">
+        <i className="fa-solid fa-star"></i>
+            </div>
+            4.93 210 reviews
+            </p>
       <div className="Catergories-rating-bar-container">
         <div className="First-category-div">
-            <div>
-                <div className="celanliness">cleanliness</div>
-                <div className="celanliness-ratingsbar"> -----
-                    <span> 5.0</span>
-                </div>
+          <div className="rating-cleanliness-container">
+            <div className="categories-label">Cleanliness</div>
+            <div className="bar-rating-label">
+              {" "}
+              -----
+              <span> 5.0</span>
             </div>
-
-            <div>Communication</div>
-            <div>Check-in</div>
+          </div>
+          <div className="rating-comunitaction-container">
+            <div className="categories-label">Comunication</div>
+            <div className="bar-rating-label">
+              {" "}
+              -----
+              <span> 5.0</span>
+            </div>
+          </div>
+          <div className="rating-checkin-container">
+            <div className="categories-label">Check-in</div>
+            <div className="bar-rating-label">
+              {" "}
+              -----
+              <span> 5.0</span>
+            </div>
+          </div>
         </div>
         <div className="Second-category-div">
-            <div>Accuracy</div>
-            <div>Location</div>
-            <div>Value</div>
+          <div className="rating-Accuracy-container">
+            <div className="categories-label">Accuracy</div>
+            <div className="bar-rating-label">
+              {" "}
+              -----
+              <span> 5.0</span>
+            </div>
+          </div>
+          <div className="rating-Location-container">
+            <div className="categories-label">Location</div>
+            <div className="bar-rating-label">
+              {" "}
+              -----
+              <span> 5.0</span>              
+            </div>
+          </div>
+          <div className="rating-Value-container">
+            <div className="categories-label">Value</div>
+            <div className="bar-rating-label">
+              {" "}
+              -----
+              <span> 5.0</span>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="REVIEWS-container">
+        <div className="Review1">box1</div>
+        <div className="Review2">box2</div>
+        <div className="Review3">box3</div>
+        <div className="Review4">box4</div>
+        <div className="Review5">box5</div>
+        <div className="Review6">box6</div>
       </div>
     </div>
   );
