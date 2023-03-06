d0d07212e46b8650c8c7eb407bcecf60c4753732
Moises Martinez <moisesmartinez@MacBook-Pro.local>
2023-03-06 10:40:47 -0600

    added a close button to my modal

--- src/Components/ShowAllReviews.jsx
+++ src/Components/ShowAllReviews.jsx
@@ -11,6 +11,9 @@ export default function ShowAllReviews() {
         <div className="modal-overlay">
           <div className="cancel-modal-container">
             <div className="cancel-modal">
+              <div className="button-container">
+                <button className="close-modal" onClick={closeAllRev}>X</button>
+              </div>
             </div>
           </div>
         </div>
