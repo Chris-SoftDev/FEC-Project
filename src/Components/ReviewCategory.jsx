9b2963e0605e6347364a5d4c373065261e58c690
Moises Martinez <moisesmartinez@MacBook-Pro.local>
2023-03-08 11:24:12 -0600

    test for bar rating

--- src/Components/ReviewCategory.jsx
+++ src/Components/ReviewCategory.jsx
@@ -1,8 +1,50 @@
 import { useContext } from "react";
 import { ReviewContext } from "../Context/ReviewContext";
-import "./ReviewCategory.css"
+import "./ReviewCategory.css";
 
 export default function ReviewCategory() {
+  const {
+    showReview,
+    getReviews,
+    cleanliness,
+    communication,
+    checkIn,
+    accuracy,
+    location,
+    value,
+    totalAvg,
+  } = useContext(ReviewContext);
+  const clean = percentageToRating(cleanliness);
+  const comu = percentageToRating(communication);
+  const check = percentageToRating(checkIn);
+  const acu = percentageToRating(accuracy);
+  const loc = percentageToRating(location);
+  const val = percentageToRating(value);
+
+  const cleanDec = roundToTenth(cleanliness);
+  const comunicationDec = roundToTenth(communication);
+  const checkDec = roundToTenth(checkIn);
+  const accuracyDec = roundToTenth(accuracy);
+  const locationDec = roundToTenth(location);
+  const valueDec = roundToTenth(value);
+
+  const totalDec = roundToTenth(totalAvg);
+
+  const totalReviews = amountOfReviews(getReviews);
+
+  function percentageToRating(num) {
+    return Math.round((num * 100) / 5);
+  }
+
+  function roundToTenth(num) {
+    return Math.round(num * 10) / 10;
+  }
+  function amountOfReviews(rev) {
+    for (let i = 0; i < rev.length; i++) {
+      const element = rev.length;
+      return element;
+    }
+  }
 
   return (
     <div className="Review-Container">
@@ -10,35 +52,37 @@ export default function ReviewCategory() {
         <div className="">
           <i className="fa-solid fa-star"></i>
         </div>
-        4.93 211 reviews
+        {totalDec} - {totalReviews} reviews
       </div>
-      <div className="Catergories-rating-bar-container">
+      <div
+        className="Catergories-rating-bar-container"
+      >
         <div className="First-category-div">
           <div className="rating-container">
             <div className="categories-label1">Cleanliness</div>
             <div className="bar-rating-label">
               <div className="progress-bar ">
-                <div className="fill"></div>
+                <div className="fill" style={{ width: `${clean}%` }}></div>
               </div>
-              <span className="avg-score"> 5.0</span>
+              <span className="avg-score">{cleanDec}</span>
             </div>
           </div>
           <div className="rating-container">
             <div className="categories-label1">Communication</div>
             <div className="bar-rating-label">
               <div className="progress-bar ">
-                <div className="fill"></div>
+                <div className="fill" style={{ width: `${comu}%` }}></div>
               </div>
-              <span className="avg-score"> 5.0</span>
+              <span className="avg-score"> {comunicationDec}</span>
             </div>
           </div>
           <div className="rating-container">
             <div className="categories-label1">Check-in</div>
             <div className="bar-rating-label">
               <div className="progress-bar ">
-                <div className="fill"></div>
+                <div className="fill" style={{ width: `${check}%` }}></div>
               </div>
-              <span className="avg-score"> 5.0</span>
+              <span className="avg-score"> {checkDec}</span>
             </div>
           </div>
         </div>
@@ -47,27 +91,27 @@ export default function ReviewCategory() {
             <div className="categories-label2">Accuracy</div>
             <div className="bar-rating-label2">
               <div className="progress-bar ">
-                <div className="fill"></div>
+                <div className="fill" style={{ width: `${acu}%` }}></div>
               </div>
-              <span className="avg-score"> 5.0</span>
+              <span className="avg-score"> {accuracyDec}</span>
             </div>
           </div>
           <div className="rating-container">
             <div className="categories-label2">Location</div>
             <div className="bar-rating-label2">
               <div className="progress-bar ">
-                <div className="fill"></div>
+                <div className="fill" style={{ width: `${loc}%` }}></div>
               </div>
-              <span className="avg-score"> 5.0</span>
+              <span className="avg-score"> {locationDec}</span>
             </div>
           </div>
           <div className="rating-container">
             <div className="categories-label2">Value</div>
             <div className="bar-rating-label2">
               <div className="progress-bar ">
-                <div className="fill"></div>
+                <div className="fill" style={{ width: `${val}%` }}></div>
               </div>
-              <span className="avg-score"> 5.0</span>
+              <span className="avg-score">{valueDec}</span>
             </div>
           </div>
         </div>
---
+++ src/Components/ReviewModalCard.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { ReviewContext } from "../Context/ReviewContext";
+import "./ReviewModalCard.css"     
+export default function ReviewCard() {
+  const { getReviews } = useContext(ReviewContext);
+  // const limitedReviews = getReviews.slice(0, 6); // Only display the first 6 reviews
+// /////////////this is the review card for the modal 
+  return (
+    <div className="Modal-REVIEWS-container">
+      {getReviews.map((review) => (
+        <div key={review.review_id} className="Modal-review-box">
+          <div className="Modal-inner-container">
+            <div className="Modal-img-name-date-container">
+              <div className="Modal-img-container">
+                <img src={review.img_url} alt="" />
+              </div>
+              <div className="Modal-name-date-container">
+                <div className="Modal-name-container">
+                  <h3 className="Modal-user">{review.user_name}</h3>
+                </div>
+                <div className="Modal-date-container">
+                  <li className="">Feburay 2023</li>
+                </div>
+              </div>
+            </div>
+            <span className="Modal-actual-review">{review.comment}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
