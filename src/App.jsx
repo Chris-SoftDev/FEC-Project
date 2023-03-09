b6cef0d70de19b878b6f6ce3e1a9a65d15854f90
Christopher Ronca <christopherronca@gmail.com>
2023-03-08 19:01:49 -0700

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -10,66 +10,52 @@ import Footer from './Components/Footer/Footer';
 import { NavProvider } from './Context/NavContext';
 import { HostProvider } from './Context/HostContext'
 import { ReviewProvider} from './Context/ReviewContext';
-import { RegionProvider } from './Context/RegionContext';
 import { LocationProvider } from './Context/LocationContext';
-import MobileNavbar from './Components/MobileNavBar';
+
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LocationShowMore from './Components/LocationShowMore';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={
-          <>
-    <div className="app-container">
-      <div className="nav-container">
-        <NavProvider>
-          <NavBar />
-        </NavProvider>
-      </div>
-      <div className="nav-mobile-container">
-        <NavProvider>
-          <MobileNavbar />
-        </NavProvider>
-      </div>
-      <div className="main-content-container">
-        <div className='rentals-content-container'>
-          <div className="rentals-container">
             <NavProvider>
-              <Rentals />
+              <ReviewProvider>
+                <div className="app-container">
+                  <div className="nav-container">
+                      <NavBar />
+                  </div>
+                  <div className="nav-mobile-container">
+                      <MobileNavbar />
+                  </div>
+                  <div className="main-content-container">
+                    <div className='rentals-content-container'>
+                      <div className="rentals-container">
+                          <Rentals />
+                      </div>
+                      <div className="reviews-container">
+                        <Reviews />
+                      </div>
+                      <div className="location-container">
+                        <LocationProvider>
+                          <Location />
+                        </LocationProvider>
+                      </div>
+                      <div className="host-container">
+                        <HostProvider>
+                          <Host />
+                        </HostProvider>
+                      </div>
+                    </div>
+                  </div>
+                  <footer className="footer-container">
+                      <Footer />
+                  </footer>
+                </div>
+              </ReviewProvider>
             </NavProvider>
-          </div>
-          <div className="reviews-container">
-          <ReviewProvider>
-            <Reviews />
-          </ReviewProvider>
-          </div>
-          <div className="location-container">
-            <LocationProvider>
-              <Location />
-            </LocationProvider>
-          </div>
-          <div className="host-container">
-            <HostProvider>
-              <Host />
-            </HostProvider>
-          </div>
-        </div>
-      </div>
-      <footer className="footer-container">
-        <RegionProvider>
-        <NavProvider>
-          <Footer />
-        </NavProvider>
-        </RegionProvider>
-      </footer>
-    </div>
-    </>
-      }/>
-        
-          <Route path='/location' element={<LocationShowMore />} />
-        
+        }/>        
+        <Route path='/location' element={<LocationShowMore />} />
       </Routes>
     </Router> 
   );
