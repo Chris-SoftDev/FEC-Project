4d1f69b77878588a2e798731f26d4e2b2a216d8e
Ben Richardson <brbooksells774@gmail.com>
2023-03-09 16:57:21 -0600

    First Rental Description added

--- src/Components/Rentals/RentalDescription.jsx
+++ src/Components/Rentals/RentalDescription.jsx
@@ -2,7 +2,36 @@ import './RentalDescription.css';
 
 function RentalDescription() {
     return (
-        <h3>Rental Description Component</h3>
+        <>
+        <div className='Rental-Box'>
+            <section className='Rental-Section'>
+                <h2>Entire cabin hosted by Tag & Sherilyn</h2>
+                <ol className='Rental-List'>
+                    <li >
+                        <span>5 guests</span>
+                    </li>
+                    <li>
+                        <span aria-hidden="true"> · </span>    
+                        <span>3 bedrooms</span>
+                    </li> 
+                    <li>
+                        <span aria-hidden="true"> · </span>
+                        <span>4 beds</span>
+                        
+                    </li>
+                    <li>
+                        <span aria-hidden="true"> · </span>    
+                        <span>2 baths</span>
+                    </li>
+                </ol>
+            
+            
+            
+            
+            
+            </section>
+        </div>
+        </>
      );
 }
 
