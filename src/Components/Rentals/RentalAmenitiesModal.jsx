8bed36d03066afb8adf00e96d2a4db8cccc1d2ad
Caleb Dewey <calebdewey@Calebs-MacBook-Pro.local>
2023-03-12 13:09:51 -0600

    made changes to seed and hostContext for amenities data

--- src/Components/Rentals/RentalAmenitiesModal.jsx
+++ src/Components/Rentals/RentalAmenitiesModal.jsx
@@ -4,7 +4,8 @@ import './RentalAmenitiesModal.css'
 
 export default function RentalAmenitiesModal() {
 
-    const { showAllAmenities, closeAmenities } = useContext(HostContext)
+    const { showAllAmenities, closeAmenities, amenities, rulesData, safetyData } = useContext(HostContext)
+    console.log(amenities)
 
     return (
         <>
@@ -117,7 +118,7 @@ export default function RentalAmenitiesModal() {
                                             <div className="amen-modal-name">
                                                 Essentials
                                                 <div className="amen-modal-name-sub">
-                                                    Towels, bed sheets, soap, and toilet paper
+                                                    
                                                 </div>
                                             </div>
                                         </div>
@@ -166,7 +167,7 @@ export default function RentalAmenitiesModal() {
                                         <div className="amen-modal-group">
                                         <svg viewBox="0 0 32 32"><path d="M9 29v-2h2v-2H6a5 5 0 0 1-4.995-4.783L1 20V8a5 5 0 0 1 4.783-4.995L6 3h20a5 5 0 0 1 4.995 4.783L31 8v12a5 5 0 0 1-4.783 4.995L26 25h-5v2h2v2zm10-4h-6v2h6zm7-20H6a3 3 0 0 0-2.995 2.824L3 8v12a3 3 0 0 0 2.824 2.995L6 23h20a3 3 0 0 0 2.995-2.824L29 20V8a3 3 0 0 0-2.824-2.995z"></path></svg>
                                             <div className="amen-modal-name">
-                                                HDTV with Hulu, Amazon Prime VclassNameeo, Apple TV, HBO Max, Netflix, Roku
+                                                HDTV with Hulu, Amazon Prime Video, Apple TV, HBO Max, Netflix, Roku
                                             </div>
                                         </div>
                                         <div className='border-bottom'></div>
