6275100489bb879f68c81b233ab05a2dc653e509
Christopher Ronca <christopherronca@gmail.com>
2023-03-13 09:43:32 -0600

    Final commit for deployment adjustments

--- src/Context/RegionContext.jsx
+++ src/Context/RegionContext.jsx
@@ -8,7 +8,7 @@ export const RegionProvider = ({ children }) => {
 
     useEffect(() => {
         const fetchCurrencies = async () => {
-            const response = await fetch('/currencies');
+            const response = await fetch('http://localhost:3000/currencies');
             const data = await response.json();
             setCurrencyData(data);
         };
@@ -18,7 +18,7 @@ export const RegionProvider = ({ children }) => {
 
     useEffect(() => {
         const fetchLanguages = async () => {
-            const response = await fetch('/languages');
+            const response = await fetch('http://localhost:3000/languages');
             const data = await response.json();
             setLanguageData(data);
         };
