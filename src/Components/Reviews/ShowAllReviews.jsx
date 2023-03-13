f2ff6f68b9ca23e53a3a8c37e234991bee7af629
Moises Martinez <moisesmartinez@MacBook-Pro.local>
2023-03-13 18:16:39 -0500

    pushing new code

--- src/Components/Reviews/ShowAllReviews.jsx
+++ src/Components/Reviews/ShowAllReviews.jsx
@@ -4,9 +4,9 @@ import "./ShowAllReviews.css";
 import ReviewModalCard from "./ReviewModalCard";
 import ReviewModalCategories from "./ReviewModalCategories";
 
-
 export default function ShowAllReviews() {
-  const { showReview, closeAllRev, totalAvg, getReviews } = useContext(ReviewContext);
+  const { showReview, closeAllRev, totalAvg, getReviews, modalReviewRef } =
+    useContext(ReviewContext);
 
   const totalDec = roundToTenth(totalAvg);
 
@@ -26,7 +26,7 @@ export default function ShowAllReviews() {
     <>
       {showReview && (
         <div className="modal-layout">
-          <div className="exit-modal-container">
+          <div className="exit-modal-container" ref={modalReviewRef}>
             <div className="exit-modal">
               <div id="close-review-btn" onClick={closeAllRev}>
                 <button>
@@ -35,29 +35,28 @@ export default function ShowAllReviews() {
                     <path d="m26 6-20 20"></path>
                   </svg>
                 </button>
-            </div>
+              </div>
               <div className="Modal-review-input-container">
                 <div className="star-review-container">
                   <div className="modal-dynamic-reviews">
-                  <i className="fa-solid fa-star fa-xs" ></i>
+                    <i className="fa-solid fa-star fa-xs"></i>
                     {totalDec} · {totalReviews} reviews
                   </div>
                   <div className="show-all-search-input-container">
                     <div className="Loupe-container">
                       <svg viewBox="0 0 32 32">
                         <g fill="none">
-                        <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
+                          <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
                         </g>
                       </svg>
                     </div>
                     <input
-                      
                       className="input"
                       type="text"
                       placeholder="Search here"
                     />
-                </div>
                   </div>
+                </div>
               </div>
               <div className="categories-and-reviews-container">
                 <div className="modal-categories">
